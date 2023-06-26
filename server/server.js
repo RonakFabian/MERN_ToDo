8ac19a1ca78fac34491211a88c2e698e17ef1b5e
RonakFabian <ronakmanfabian@gmail.com>
2023-06-26 22:24:55 +0530

    Server Init . CRUD operation with MongoDB

---
+++ server/server.js
@@ -0,0 +1,117 @@
+const { json } = require("body-parser");
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const Product = require("./model/productModel");
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) =>
+{
+  res.send("HEY");
+})
+
+app.get("/product", async (req, res) =>
+{
+  try
+  {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error)
+  {
+    console.log(error);
+    res.status(404).json(error);
+  }
+}
+)
+
+app.get("/product/:id", async (req, res) =>
+{
+  try
+  {
+
+    const products = await Product.findById(req.params.id);
+    res.status(200).json(products);
+  } catch (error)
+  {
+    console.log(error);
+    res.status(404).json(error);
+  }
+}
+)
+
+app.put("/product/:id", async (req, res) =>
+{
+  try
+  {
+
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body);
+
+    if (!product)
+    {
+      return res.status(404).json("Product Not Found");
+
+    }
+    const updatedProduct = await Product.findById(req.params.id);
+    res.status(200).json(updatedProduct);
+  } catch (error)
+  {
+    console.log(error);
+    res.status(404).json(error);
+  }
+})
+
+app.post("/product", async (req, res) =>
+{
+  try
+  {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+
+
+  } catch (error)
+  {
+    console.log(error)
+    res.status(404).send(error);
+  }
+})
+
+
+app.delete("/product/:id", async (req, res) =>
+{
+
+  try
+  {
+
+    const product = await Product.findByIdAndDelete(req.params.id);
+
+    if (!product)
+    {
+      return res.status(404).json("Product Not Found");
+
+    }
+    res.status(200).json(product);
+  } catch (error)
+  {
+    console.log(error);
+    res.status(404).json(error);
+  }
+
+})
+
+
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb+srv://admin:12345@cluster0.0fgonrx.mongodb.net/MERN_ToDo")
+  .then(() =>
+  {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () =>
+    {
+      console.log("Server running on:" + PORT);
+    })
+  })
+  .catch(e => console.log(e));
